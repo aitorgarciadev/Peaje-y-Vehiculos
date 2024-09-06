a9c2a57e1e0e2cd69dcd0cfedf391729c470e4f5
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-09-06 18:16:53 +0200

    TollService was implanted

--- src/main/java/dev/aitor/toll/service/TollService.java
+++ src/main/java/dev/aitor/toll/service/TollService.java
@@ -1,5 +1,22 @@
 package dev.aitor.toll.service;
 
+import dev.aitor.toll.model.TollStation;
+import dev.aitor.toll.model.Vehicle;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TollService {
-    
+    private final TollStation tollStation;
+
+    public TollService() {
+        this.tollStation = new TollStation("Central Toll", "Springfield");
+    }
+
+    public void processVehicle(Vehicle vehicle) {
+        tollStation.processVehicle(vehicle);
+    }
+
+    public TollStation getTollStation() {
+        return tollStation;
+    }
 }
